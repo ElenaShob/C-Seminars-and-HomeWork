9612096f9e64828d221aa32397c1baf432973df1
Alena <shob@mail.ru>
2022-12-22 22:59:58 +0300

    черновик

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int NumberLength(int number)
+{
+int count = 1;
+int result = 1;
+While (result > 0)
+{
+    result = number / 10;
+    count++;
+}
+return count;
+}
+
+bool Polyndrom(int number)
+{
+    
+}
