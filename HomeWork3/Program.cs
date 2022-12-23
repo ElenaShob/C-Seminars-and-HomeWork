fafe494d8b648015db5000952cea04368fb09251
Alena <shob@mail.ru>
2022-12-23 23:54:58 +0300

    черновик 2

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,17 +1,35 @@
-﻿
-int NumberLength(int number)
-{
-int count = 1;
-int result = 1;
-While (result > 0)
-{
-    result = number / 10;
-    count++;
-}
-return count;
-}
+﻿int number = 1234321;
+int d = 9;
+int n = 0;
+int x = 1;
+int a = 1;
+int b = 1;
+while (n == 0)
+    {
+    n = Convert.ToInt32(number / Math.Pow(10,d));
+    d = d-1;
+    }
+a = Convert.ToInt32(number / Math.Pow(10,(d+1)));
+b = Convert.ToInt32(number % Math.Pow(10,x));
 
-bool Polyndrom(int number)
-{
-    
-}
+if (a == b) 
+     {
+         while (x <= d+1)
+          {
+                a = Convert.ToInt32(number / Math.Pow(10,d+1)%10);
+                b = Convert.ToInt32(number % Math.Pow(10,x)/Math.Pow(10,(x-1)));
+                d = d - 1;
+                x = x + 1;
+                Console.Write("n = " + n + " ");
+                Console.Write("d = " + d + " ");
+                Console.Write("a = " + a + " ");
+                Console.WriteLine("b = " + b + " ");
+          } 
+     }
+     else  
+     {
+        Console.WriteLine("-1");
+     }
+       
+
+
